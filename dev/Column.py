58ed128a3ce5b21282479af04ac058d981e6f617
tiredpiru <martin031101@o.cnu.ac.kr>
2023-12-12 19:48:14 +0900

    Column end 기능 구현 + 구현 기능을 팩토리 메서드화 시킴

--- dev/Column.py
+++ dev/Column.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 from dev.Color import Color
 from dev.view import View
-
+from dev.AxisAlignment import AxisAlignment
 
 class Column(View):
     def __init__(
@@ -11,16 +11,18 @@ class Column(View):
             height: int,
             background: Color = Color(0, 0, 0, 0),
             children: list[View] = [],
+            main_axis_alignment = AxisAlignment.start
         ):
         super().__init__(width, height, background=background)
         self.__children = children
         self.__coordinate: int = 0
+        self.__main_axis_alignment = main_axis_alignment
 
     def generate(self) -> Image:
         children_image = [i.generate() for i in self.__children]
         background = self._make_background()
-        for image in children_image:
-            background.paste(image, (0, self.__coordinate))
-            self.__coordinate += image.size[1]
-    
+        y_coordinates = self.__main_axis_alignment.value.make(self._height, [i.get_height() for i in self.__children])
+        for i in range(len(children_image)):
+            background.paste(children_image[i], (0, y_coordinates[i]))
+
         return background
--- test/test_column.py
+++ test/test_column.py
@@ -1,9 +1,9 @@
-from unittest import *
 import unittest
+from unittest import *
 
+from dev.AxisAlignment import AxisAlignment
 from dev.Color import Colors
 from dev.Column import Column
-from dev.padding import Padding
 from dev.view import View
 from test_view import TestView
 
@@ -42,6 +42,74 @@ class TestColumn(TestCase):
         self.assertTrue(TestView.is_section_colored(0, 100, 100, 150, image, Colors.green))
         self.assertTrue(TestView.is_section_colored(0, 150, 100, 250, image, Colors.blue))
         self.assertTrue(TestView.is_section_colored(0, 250, 100, 300, image, Colors.white))
+
+    def test_column_MainAxisAlignment_end(self):
+        """
+        MainAxisAlignment_end를 할 경우, 제일 밑에서 위로 올라가며 생성되는가?
+        """
+        column = Column(
+            width=100,
+            height=300,
+            background=Colors.white,
+            children=[
+                View(
+                    width=100,
+                    height=100,
+                    background=Colors.black,
+                ),
+                View(
+                    width=100,
+                    height=100,
+                    background=Colors.blue,
+                ),
+            ],
+            main_axis_alignment=AxisAlignment.end,
+        )
+
+        image = column.generate()
+        # 위 100픽셀이 white?
+        self.assertTrue(TestView.is_section_colored(0, 0, 100, 100, image, Colors.white))
+        # 중간 픽셀이 black?
+        self.assertTrue(TestView.is_section_colored(100, 0, 100, 200, image, Colors.black))
+        # 마지막 100픽셀이 blue?
+        self.assertTrue(TestView.is_section_colored(200, 0, 100, 300, image, Colors.blue))
+
+    def test_column_MainAxisAlignment_space_around(self):
+        """
+        mainAxisAlignment을 space around로 하게 되면,
+        객체간은 같은 거리, 객체와 벽 사이는 절반의 거리를 가지게 되는가?
+        :return:
+        """
+        column = Column(
+            width=100,
+            height=600,
+            background=Colors.white,
+            children=[
+                View(
+                    width=100,
+                    height=100,
+                    background=Colors.red,
+                ),
+                View(
+                    width=100,
+                    height=100,
+                    background=Colors.green,
+                ),
+                View(
+                    width=100,
+                    height=100,
+                    background=Colors.blue,
+                )
+            ],
+            main_axis_alignment=AxisAlignment.space_around
+        )
+
+        image = column.generate()
+        target_color = [Colors.white, Colors.red, Colors.white, Colors.green, Colors.white, Colors.blue, Colors.white]
+        target_y = [0, 50, 150, 250, 350, 450, 550, 600]
+        for i in range(len(target_color)):
+            self.assertTrue(TestView.is_section_colored(0, target_y[i], 100, target_y[i+1], image, target_color[i]))
+
 
 if __name__ == "main":
     unittest.main()
